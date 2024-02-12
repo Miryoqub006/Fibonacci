0adb04ee46c2351ece56be9cffe28ef33cdebe73
Miryoqub <salimovmiryoqub@gmail.com>
2024-02-12 20:59:51 +0500

    FIBO

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿int n = Convert.ToInt32(Console.ReadLine());
+        int a = 0;
+        int b = 1;
+        int c;
+
+        Console.WriteLine("Fibonacci sonlari:");
+        Console.Write(a + ", " + b + ", ");
+
+        for (int i = 2; i < n; i++)
+        {
+            c = a + b;
+            Console.Write(c + ", ");
+            a = b;
+            b = c;
+        }
